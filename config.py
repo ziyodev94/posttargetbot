0b31ded0102f99daca3160822cf848a64837f63e
ziyodev94 <ziyopochta@gmail.com>
2025-07-09 23:53:27 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,16 @@
-# config.py
+import os
 
-API_ID = 24290472
-API_HASH = "236cc81cadba1d070fd7a3c1a700c1dc"
+API_ID = int(os.environ.get("API_ID", "0"))
+API_HASH = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_OWNER_ID = int(os.environ.get("BOT_OWNER_ID", "0"))
 
-SESSION_NAME_REPLY = "reply_sync_session"
-SESSION_NAME_CHECK = "delete_checker_session"
+SESSION_NAME_REPLY = os.environ.get("SESSION_NAME_REPLY", "reply_sync_session")
+SESSION_NAME_CHECK = os.environ.get("SESSION_NAME_CHECK", "delete_checker_session")
 
-BOT_TOKEN = "7785607881:AAGvkgx9fX5tIk5ONvymxLxOgJ-mEeRVzxY"
-BOT_OWNER_ID = 1293329834
+MAIN_CHANNEL_ID = int(os.environ.get("MAIN_CHANNEL_ID", "0"))
+TARGET_CHAT_IDS = list(map(int, os.environ.get("TARGET_CHAT_IDS", "").split(",")))
 
-MAIN_CHANNEL_ID = -1002776358214
-TARGET_CHAT_IDS = [-1002721804024, -1002873822791]
-
-MAPPING_FILE = "post_map.json"
-LAST_UPDATE_ID_FILE = "last_update_id.txt"
-TEXT_FORWARD_CONFIG = "text_forwarding_enabled.json"
+MAPPING_FILE = os.environ.get("MAPPING_FILE", "post_map.json")
+LAST_UPDATE_ID_FILE = os.environ.get("LAST_UPDATE_ID_FILE", "last_update_id.txt")
+TEXT_FORWARD_CONFIG = os.environ.get("TEXT_FORWARD_CONFIG", "text_forwarding_enabled.json")
